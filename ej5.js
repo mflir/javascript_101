8eb6b6fbbe745f021e0bfd59574fe9a2e6c69432
mflir <scavone_f@hotmail.com>
2017-10-19 21:12:10 -0300

    hola

---
+++ ej5.js
@@ -0,0 +1,10 @@
+/* Declarar una variable que se llame ganeLaCarrera y asignarle el valor false. 
+Mostrar por la terminal un mensaje que diga 'Ganaste' si ganeLaCarrera es verdadero y 'Perdiste' 
+si su valor es falso. */
+
+var ganeLaCarrera = false;
+if ganeLaCarrera = true {
+	console.log("Ganaste");
+}else{
+	console.log("Perdiste");
+}
